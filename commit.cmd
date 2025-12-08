408c3a151ae7201129c3e0408e01d98bb3588ce0
Dirk Arnez <smalldirkalex@gmail.com>
2025-12-08 11:47:32 +0800

    - update commit.cmd

--- commit.cmd
+++ commit.cmd
@@ -6,5 +6,5 @@ SET PATH=^
 %DOWNLOADS_DIR%\PortableGit\bin;
 
 
-start "" "%DOWNLOADS_DIR%\tortoisegit-portable-v2.13.0.1\TortoiseGitProc.exe" /command:commit /path:"%~1"
+start "" "%DOWNLOADS_DIR%\tortoisegit-portable-v2.13.0.1\bin\TortoiseGitProc.exe" /command:commit /path:"%~1"
 pause
